f1135bf5c58140884a08bd13821696302be91a13
roarkmccolgan <roarkmccolgan@gmail.com>
2018-07-04 15:30:56 +0200

    gallery

--- app/Product.php
+++ app/Product.php
@@ -42,6 +42,7 @@ class Product extends Model implements HasMedia
                 ->addMediaConversion('thumb')
                 ->width(200)
                 ->height(200);
+            });
             $this
                 ->addMediaConversion('product')
                 ->width(400)
