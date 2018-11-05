502d3387db2dea1babca46e16b7a23b2c75f0e94
roarkmccolgan <roarkmccolgan@gmail.com>
2018-11-05 13:46:27 +0200

    Update menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer(['partial.mainav','home'], function($view){
-            $categories = Category::with(['products','allSubCategories.products'])->orderBy('order')->where('parent_id',null)->where('id', '!=', 11)->get();
+            $categories = Category::with(['products','allSubCategories.products'])->orderBy('order')->where('parent_id',null)->where([
+                ['id', '!=', '11'],
+                ['id', '!=', '12'],
+            ])->get();
             $casestudycategories = Category::has('casestudies')->get();
             $casestudies = CaseStudy::with(['category','siteproducts'])->take(3)->latest()->get();
             $news = News::with(['category','siteproducts'])->whereDate('publish', '<=', Carbon::now())->get();
