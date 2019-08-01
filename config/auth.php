7338ee096be7c6321a67024cd39f0a72d3b9f334
roarkmccolgan <roarkmccolgan@gmail.com>
2019-08-01 15:32:26 +0200

    Changes done

--- config/auth.php
+++ config/auth.php
@@ -65,13 +65,14 @@ return [
     */
 
     'providers' => [
+        'users' => [
+            'driver' => 'auth0',
+        ],
+        
         // 'users' => [
         //     'driver' => 'eloquent',
         //     'model' => App\User::class,
         // ],
-        'users' => [
-            'driver' => 'auth0',
-        ],
 
         // 'users' => [
         //     'driver' => 'database',
