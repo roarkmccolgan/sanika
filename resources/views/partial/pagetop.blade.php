a757d88b21b56a9c83614cb241ada8fdfbd1c237
roarkmccolgan <roarkmccolgan@gmail.com>
2019-08-02 13:44:05 +0200

    articel updates

--- resources/views/partial/pagetop.blade.php
+++ resources/views/partial/pagetop.blade.php
@@ -1,20 +1,18 @@
-<div class="bg-black py-2 text-white text-sm font-bold relative z-50">
-	@if(url()->current() == 'http://sanikacc.test' || url()->current() == 'http://sanika.co.za')
-	<div class="sm:absolute z-50 sm:pin-x flex justify-center h-20 overflow-hidden pointer-events-none">
-		<transition name="slide">
-			<div ref="newsItem" class="hidden p-2 w-full sm:w-2/5 text-white bg-white rounded-b border-b border-grey shadow pointer-events-auto" style="background: rgb(201,0,0);background: linear-gradient(0deg, rgba(201,0,0,1) 40%, rgba(136,45,45,1) 90%, rgba(89,0,0,1) 100%);" v-show="showNews" v-cloak>
-		        <div class="text-sm">Latest Article:</div>
-		        <p class="font-bold uppercase">{{ $news->first()->title  }}</p>
-		        <a href="/news/{{ $news->first()->category->alias }}/{{ $news->first()->alias }}" class="text-sm text-white">read now</a>
-			</div>
-		</transition>
-	</div>
-	@endif
-	<div class="container mx-auto flex items-center justify-between px-2 sm:px-0">
+<div class="bg-black py-2 text-white text-sm font-bold relative z-50 sm:h-10 sm:overflow-visible">
+		
+	<div class="container mx-auto flex flex-wrap items-start justify-between px-2 sm:px-0">
 		<div class="flex items-center">
 			<div class="pr-4 mr-4 text-xs sm:text-sm">Call: <a class="no-underline text-sanika-primary" href="tel:+274243061">+27 (011) 425 3061</a></div>
 			<div class="pr-4 mr-4 text-xs sm:text-sm">Email: <a class="no-underline text-sanika-primary" href="mailto:info@sanika.co.za">info@sanika.co.za</a></div>
 		</div>
+		@if(url()->current() == 'http://sanikacc.test' || url()->current() == 'http://sanika.co.za')
+		<transition name="slide">
+			<a href="/news/{{ $news->first()->category->alias }}/{{ $news->first()->alias }}" ref="newsItem" class="newsItem sm:order-none w-full sm:flex-1 hidden no-underline mt-2 mr-0 sm:mr-8 sm:mt-0 p-2 w-full sm:w-2/5 text-white bg-white rounded-b border-b border-grey shadow pointer-events-auto" style="background: rgb(201,0,0);background: linear-gradient(0deg, rgba(201,0,0,1) 40%, rgba(136,45,45,1) 90%, rgba(89,0,0,1) 100%);" v-show="showNews" v-cloak>
+		        <div class="text-sm">Latest Article:</div>
+		        <p class="font-bold uppercase">{{ $news->first()->title  }}</p>
+			</a>
+		</transition>
+		@endif
 		<div class="font-normal text-sm">
 			@if($isLoggedIn)
 			<div class="flex items-center">
