d4920a7f83fca0cb8f1e66c6da470d0cbb3c46fc
roarkmccolgan <roarkmccolgan@gmail.com>
2019-08-02 13:16:57 +0200

    articel updates

--- resources/views/partial/pagetop.blade.php
+++ resources/views/partial/pagetop.blade.php
@@ -1,8 +1,8 @@
 <div class="bg-black py-2 text-white text-sm font-bold relative z-50">
 	@if(url()->current() == 'http://sanikacc.test' || url()->current() == 'http://sanika.co.za')
-	<div class="absolute z-50 pin-x flex justify-center h-20 overflow-hidden pointer-events-none">
+	<div class="sm:absolute z-50 sm:pin-x flex justify-center h-20 overflow-hidden pointer-events-none">
 		<transition name="slide">
-			<div ref="newsItem" class="hidden p-2 w-1/5 text-white bg-white rounded-b border-b border-grey shadow pointer-events-auto" style="background: rgb(201,0,0);background: linear-gradient(0deg, rgba(201,0,0,1) 40%, rgba(136,45,45,1) 90%, rgba(89,0,0,1) 100%);" v-show="showNews" v-cloak>
+			<div ref="newsItem" class="hidden p-2 w-full sm:w-2/5 text-white bg-white rounded-b border-b border-grey shadow pointer-events-auto" style="background: rgb(201,0,0);background: linear-gradient(0deg, rgba(201,0,0,1) 40%, rgba(136,45,45,1) 90%, rgba(89,0,0,1) 100%);" v-show="showNews" v-cloak>
 		        <div class="text-sm">Latest Article:</div>
 		        <p class="font-bold uppercase">{{ $news->first()->title  }}</p>
 		        <a href="/news/{{ $news->first()->category->alias }}/{{ $news->first()->alias }}" class="text-sm text-white">read now</a>
